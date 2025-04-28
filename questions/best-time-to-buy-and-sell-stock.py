84bbe7a09ddd738766dd1d336fc53056d1e9cd58
Fatima Salama <ffas92@gmail.com>
2025-04-28 14:13:06 +0300

    implement best-time-to-buy-and-sell-stock solution in PHP and Python

---
+++ questions/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+class Solution():
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        minPrice = 100000
+        maxProfit = 0
+
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
+            elif price - minPrice > maxProfit:
+                maxProfit = price - minPrice
+        return maxProfit
